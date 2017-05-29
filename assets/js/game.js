8d900427a06fbf75c7d1a7eedadc49b6176bdc52
Ben Charais <benjcharais@gmail.com>
2017-05-29 00:39:42 -0600

    Finishing up multiple gmaeplay logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,4 @@
+var dead = false;
 var character = false;
 var enemy = false;
 var taunts = ["She's a witch!", "It's just a flesh wound", "Go away or I shall taunt you a second time!", "Go and boil your bottoms!", "I blow my nose at you! You and all your silly English K-nigh-hts!", "If you think you got a nasty taunting this time, you ain't heard nothing yet!", "You don't frighten me with your silly knees-bent running around advancing behavior!", "NI!", "You have the brain of a duck!", "BURRRNNNN HERRRRRR", "Fetch us the Holy Hand Grenade!", "You English bedwetting types", "You tiny-brained wipers of other people's bottoms!"]
@@ -9,21 +10,26 @@ var imNotDead;
 var imVeryDead;
 var wins = 0;
 
-function Character(attack, counter, health) {
+function Character(base, attack, counter, health) {
+	this.base = base;
 	this.attack = attack;
 	this.counter = counter;
 	this.health = health;
 }
 
 function characterSelect(unit) {
+	$("#combat").empty();
 	imNotDead = $(unit).parent();
 	$(unit).toggleClass("char character");
-	$("#character").append(unit);
+	$("#character").html(unit);
 	character = true;
 	unitBuilder(selected, 0);
+	$("#instr").text("I guess they're okay, now choose an enemy!");
+	$("#charhp").text("Health: " + chosenCharacter.health);
 }
 
 function enemySelect(unit) {
+	dead = false;
 	if ( $(unit).hasClass("character") ) {
 		return;
 	}
@@ -33,38 +39,34 @@ function enemySelect(unit) {
 	enemy = true;
 	unitBuilder(selected, 1);
 	$("#taunter").css("visibility", "visible");
+	$("#instr").text("They look pretty tough, watch out!");
+	$("#enemyhp").text("Health: " + currentEnemy.health);
 }
 
 function unitBuilder(selected, test) {
 	switch(selected) {
 		case "robin":
-			base = 15;
-			alignment = new Character(base, 5, 35);
+			alignment = new Character(16, 16, 11, 90);
 			break;
 
 		case "black":
-			base = 30;
-			alignment = new Character(base, 10, 45);
+			alignment = new Character(32, 32, 16, 80);
 			break;
 
 		case "enchanter":
-			base = 40;
-			alignment = new Character(base, 5, 50);
+			alignment = new Character(38, 38, 9, 85);
 			break;
 
 		case "french":
-			base = 10;
-			alignment = new Character(base, 15, 20);
+			alignment = new Character(17, 17, 21, 90);
 			break;
 
 		case "rabbit":
-			base = 10;
-			alignment = new Character(base, 20, 60);
+			alignment = new Character(10, 10, 24, 110);
 			break;
 
 		case "peasants":
-			base = 5;
-			alignment = new Character(base, 5, 80);
+			alignment = new Character(5, 5, 2.5, 211);
 			break;
 		
 		default:
@@ -78,43 +80,58 @@ function unitBuilder(selected, test) {
 }
 
 function taunt() {
+	//Controls whether or not you win on last hit of a fight
 	console.log(taunts[Math.floor(Math.random() * taunts.length)]);
-	currentEnemy.health -= chosenCharacter.attack;
 	
-	chosenCharacter.attack += base;
-
-	if (currentEnemy.health <= 0) {
-		winFight(true);
-		
-	} 
-
+	//Combat exchange using object properties
+	currentEnemy.health -= chosenCharacter.attack;
+	$("#enemyhp").text("Health: " + currentEnemy.health);
+	chosenCharacter.attack += chosenCharacter.base;
+	$("#combat").html("You taunted them for a pitiful " + chosenCharacter.attack + " damage. <br> They taunted you right back for " + currentEnemy.counter + " damage.");
 	chosenCharacter.health -= currentEnemy.counter;
+	$("#charhp").text("Health: " + chosenCharacter.health);
 
-	if (chosenCharacter.health <= 0) {
-		//alert to determine play again
-		winFight(false);
-			
+
+	//Check if enemy is dead first then player
+	if (currentEnemy.health <= 0) {
+		$("#instr").html("That was nasty, you kiss your mother with that mouth? <br> Choose your next victim!");
+		$("#enemyhp").empty();
+		winFight(true);
+	} else if (chosenCharacter.health <= 0) {
+		$("#charhp").text("Health: 0");
+		var agree = confirm("You were taunted quite viciously and had to run away.\nWould you like to play again?")
+		if (agree) {
+			winFight(false);
+		} else {
+			character = true;
+			enemy = true;
+			$("#taunter").css("visibility", "hidden");
+			$("#instr").text("Oh had enough have you?");
 		}
-			
 	}
-
+			
+}	
 
 function winFight(winState) {
 	if (winState) {
 		$("#taunter").css("visibility", "hidden");
 		$("#enemy > div").toggleClass("char enemy");
-		$(imVeryDead).append($("#enemy > div"));
+		$(imVeryDead).html($("#enemy > div"));
 		$(imVeryDead).css("visibility", "hidden");
 		enemy = false;
 		imVeryDead = null;
 		wins++;
-		if (wins === 5) {
+		if (wins === 5 && chosenCharacter.health >= 0) {
+			dead = true;
 			winGame();
 		}
 	} else {
 		$("#character > div").toggleClass("char character");
-		$(imNotDead).append($("#character > div"));
+		$(imNotDead).html($("#character > div"));
+		$("#charhp").empty();
 		$("#enemy").empty();
+		$(".char").css("visibility", "visible");
+		$("#instr").html("Tis just a flesh wound.")
 		enemy = false;
 		character = false;
 	}
@@ -122,10 +139,20 @@ function winFight(winState) {
 
 function winGame() {
 	$(document).ready(function() {
-		alert("you won! woo");
+		$("#combat").html("Congratulations, you have taunted them fiercely.");
+		var agree = confirm("Would you like to continue your search for the grail?");
+		if (agree) {
+			$(".char").css("visibility", "visible");
+			$("#character > div").toggleClass("char character");
+			$(imNotDead).append($("#character > div"));
+			character = false;
+			enemy = false;
+			$("#charhp").empty();
+			$("#instr").html("Choose your new champion!");
+			wins = 0;
+		}
 	});
 }
-
 
 $(document).ready(function() {
 
